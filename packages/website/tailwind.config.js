4a8b9d3f973a538f9a37149a5e63b215fb9af197
anuraghazra <hazru.anurag@gmail.com>
2021-03-30 16:51:08 +0530

    chore: minor updates

--- packages/website/tailwind.config.js
+++ packages/website/tailwind.config.js
@@ -1,7 +1,10 @@
 const preset = require("@renderlesskit/react-tailwind/preset");
 
 module.exports = preset({
-  purge: [],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},

ac5b93662a1b65b7ff038914f9e1749fbd9d67b7
anuraghazra <hazru.anurag@gmail.com>
2021-03-30 19:48:32 +0530

    fix: added tailwindjit & fixed purgecss

--- packages/website/tailwind.config.js
+++ packages/website/tailwind.config.js
@@ -1,7 +1,11 @@
 const preset = require("@renderlesskit/react-tailwind/preset");
 
 module.exports = preset({
-  purge: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../node_modules/\\@renderlesskit/react-tailwind/dist/esm/theme/defaultTheme/*.js",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},

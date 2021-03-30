e08e5beecdaf27051ceb6c3c29ee000f62d66bae
anuraghazra <hazru.anurag@gmail.com>
2021-03-30 17:10:50 +0530

    feat: added support to remove auto anchors

--- packages/nextra-theme-docs/src/index.js
+++ packages/nextra-theme-docs/src/index.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useMemo, useContext, createContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useContext,
+  createContext,
+} from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
@@ -31,7 +37,9 @@ const MenuContext = createContext(false);
 const getFSRoute = (asPath, locale) => {
   if (!locale) return asPath.replace(new RegExp("/index(/|$)"), "$1");
 
-  return asPath.replace(new RegExp(`\.${locale}(\/|$)`), "$1").replace(new RegExp("/index(/|$)"), "$1");
+  return asPath
+    .replace(new RegExp(`\.${locale}(\/|$)`), "$1")
+    .replace(new RegExp("/index(/|$)"), "$1");
 };
 
 function Folder({ item, anchors }) {
@@ -77,9 +85,21 @@ function File({ item, anchors }) {
   const slugger = new Slugger();
   const activeAnchor = useActiveAnchor();
 
-  const title = item.title;
-  // if (item.title.startsWith('> ')) {
-  // title = title.substr(2)
+  const LI = ({ title }) => (
+    <li className={active ? "active" : ""}>
+      <Link href={item.route}>
+        <a onClick={() => setMenu(false)}>{title}</a>
+      </Link>
+    </li>
+  );
+
+  let title = item.title;
+  if (item.title.startsWith("> ")) {
+    title = title.substr(2);
+    return <LI title={title} />;
+  }
+
+  // console.log({ anchors, title, item });
   if (anchors && anchors.length) {
     if (active) {
       let activeIndex = 0;
@@ -109,10 +129,10 @@ function File({ item, anchors }) {
                   onClick={() => setMenu(false)}
                   className={isActive ? "active-anchor" : ""}
                 >
-                  <span className='flex text-sm'>
-                    <span className='opacity-25'>#</span>
-                    <span className='mr-2'></span>
-                    <span className='inline-block'>{text}</span>
+                  <span className="flex text-sm">
+                    <span className="opacity-25">#</span>
+                    <span className="mr-2"></span>
+                    <span className="inline-block">{text}</span>
                   </span>
                 </a>
               );
@@ -123,13 +143,7 @@ function File({ item, anchors }) {
     }
   }
 
-  return (
-    <li className={active ? "active" : ""}>
-      <Link href={item.route}>
-        <a onClick={() => setMenu(false)}>{title}</a>
-      </Link>
-    </li>
-  );
+  return <LI title={title} />;
 }
 
 function Menu({ dir, anchors }) {
@@ -156,7 +170,7 @@ function Sidebar({ show, directories, anchors }) {
         height: "calc(100vh - 4rem)",
       }}
     >
-      <div className='w-full h-full p-4 pb-40 overflow-y-auto border-gray-200 sidebar dark:border-gray-900 md:pb-16'>
+      <div className="w-full h-full p-4 pb-40 overflow-y-auto border-gray-200 sidebar dark:border-gray-900 md:pb-16">
         <Menu dir={directories} anchors={anchors} />
       </div>
     </aside>
@@ -164,25 +178,30 @@ function Sidebar({ show, directories, anchors }) {
 }
 
 const Layout = ({ filename, config: _config, pageMap, meta, children }) => {
+  console.log(filename, _config, pageMap, meta);
   const [menu, setMenu] = useState(false);
   const router = useRouter();
   const { route, asPath, locale, defaultLocale } = router;
   const fsPath = getFSRoute(asPath, locale);
 
-  const directories = useMemo(() => cleanupAndReorder(pageMap, locale, defaultLocale), [
-    pageMap,
-    locale,
-    defaultLocale,
-  ]);
-  const flatDirectories = useMemo(() => flatten(directories), [directories]);
+  const directories = useMemo(
+    () => cleanupAndReorder(pageMap, locale, defaultLocale),
+    [pageMap, locale, defaultLocale]
+  );
+  const flatDirectories = useMemo(() => flatten([directories]), [directories]);
   const config = Object.assign({}, defaultConfig, _config);
 
   const filepath = route.slice(0, route.lastIndexOf("/") + 1);
   const filepathWithName = filepath + filename;
-  const titles = React.Children.toArray(children).filter((child) => titleType.includes(child.props.mdxType));
+  const titles = React.Children.toArray(children).filter((child) =>
+    titleType.includes(child.props.mdxType)
+  );
   const titleEl = titles.find((child) => child.props.mdxType === "h1");
-  const title = meta.title || (titleEl ? innerText(titleEl.props.children) : "Untitled");
-  const anchors = titles.filter((child) => child.props.mdxType === "h2").map((child) => child.props.children);
+  const title =
+    meta.title || (titleEl ? innerText(titleEl.props.children) : "Untitled");
+  const anchors = titles
+    .filter((child) => child.props.mdxType === "h2")
+    .map((child) => child.props.children);
 
   useEffect(() => {
     if (menu) {
@@ -192,10 +211,10 @@ const Layout = ({ filename, config: _config, pageMap, meta, children }) => {
     }
   }, [menu]);
 
-  const currentIndex = useMemo(() => flatDirectories.findIndex((dir) => dir.route === fsPath), [
-    flatDirectories,
-    fsPath,
-  ]);
+  const currentIndex = useMemo(
+    () => flatDirectories.findIndex((dir) => dir.route === fsPath),
+    [flatDirectories, fsPath]
+  );
 
   const isRTL = useMemo(() => {
     if (!config.i18n) return config.direction === "rtl" || null;
@@ -206,7 +225,9 @@ const Layout = ({ filename, config: _config, pageMap, meta, children }) => {
   return (
     <React.Fragment>
       <Head>
-        {config.font ? <link rel='stylesheet' href='https://rsms.me/inter/inter.css' /> : null}
+        {config.font ? (
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        ) : null}
         <title>
           {title}
           {renderComponent(config.titleSuffix, { locale })}
@@ -226,49 +247,81 @@ const Layout = ({ filename, config: _config, pageMap, meta, children }) => {
           rtl: isRTL,
         })}
       >
-        <nav className='fixed top-0 left-0 right-0 z-20 flex items-center h-16 px-6 bg-white border-b border-gray-200 dark:bg-dark dark:border-gray-900'>
-          <div className='flex items-center hidden w-full md:block'>
-            <Link href='/'>
-              <a className='inline-block text-current no-underline hover:opacity-75'>
+        <nav className="fixed top-0 left-0 right-0 z-20 flex items-center h-16 px-6 bg-white border-b border-gray-200 dark:bg-dark dark:border-gray-900">
+          <div className="flex items-center hidden w-full md:block">
+            <Link href="/">
+              <a className="inline-block text-current no-underline hover:opacity-75">
                 {renderComponent(config.logo, { locale })}
               </a>
             </Link>
           </div>
 
           {config.customSearch ||
-            (config.search ? config.UNSTABLE_stork ? <StorkSearch /> : <Search directories={flatDirectories} /> : null)}
+            (config.search ? (
+              config.UNSTABLE_stork ? (
+                <StorkSearch />
+              ) : (
+                <Search directories={flatDirectories} />
+              )
+            ) : null)}
 
-          <div className='mr-2' />
+          <div className="mr-2" />
 
           {config.darkMode ? <ThemeSwitch /> : null}
 
-          {config.i18n ? <LocaleSwitch options={config.i18n} isRTL={isRTL} /> : null}
+          {config.i18n ? (
+            <LocaleSwitch options={config.i18n} isRTL={isRTL} />
+          ) : null}
 
           {config.repository ? (
-            <a className='p-2 text-current' href={config.repository} target='_blank'>
+            <a
+              className="p-2 text-current"
+              href={config.repository}
+              target="_blank"
+            >
               <GitHubIcon height={24} />
             </a>
           ) : null}
 
-          <button className='block p-2 md:hidden' onClick={() => setMenu(!menu)}>
-            <svg fill='none' width='24' height='24' viewBox='0 0 24 24' stroke='currentColor'>
-              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
+          <button
+            className="block p-2 md:hidden"
+            onClick={() => setMenu(!menu)}
+          >
+            <svg
+              fill="none"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
             </svg>
           </button>
 
-          <div className='-mr-2' />
+          <div className="-mr-2" />
         </nav>
         <ActiveAnchor>
-          <div className='flex flex-1 h-full'>
+          <div className="flex flex-1 h-full">
             <MenuContext.Provider value={{ setMenu }}>
-              <Sidebar show={menu} anchors={anchors} directories={directories} />
+              <Sidebar
+                show={menu}
+                anchors={anchors}
+                directories={directories}
+              />
             </MenuContext.Provider>
             <SkipNavContent />
             {meta.full ? (
-              <article className='relative w-full pt-16 overflow-x-hidden'>{children}</article>
+              <article className="relative w-full pt-16 overflow-x-hidden">
+                {children}
+              </article>
             ) : (
-              <article className='relative w-full max-w-full px-6 pt-20 pb-16 overflow-x-hidden docs-container md:px-8'>
-                <main className='max-w-screen-md mx-auto'>
+              <article className="relative w-full max-w-full px-6 pt-20 pb-16 overflow-x-hidden docs-container md:px-8">
+                <main className="max-w-screen-md mx-auto">
                   <Theme>{children}</Theme>
                   <Footer
                     config={config}
@@ -289,7 +342,7 @@ const Layout = ({ filename, config: _config, pageMap, meta, children }) => {
 
 export default (opts, config) => (props) => {
   return (
-    <ThemeProvider attribute='class'>
+    <ThemeProvider attribute="class">
       <Layout config={config} {...opts} {...props} />
     </ThemeProvider>
   );
